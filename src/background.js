74c34c80554fd7b55c90a960a2549f8a943863eb
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-03-05 19:57:34 +0300

    Format the javascript using Prettier

--- src/background.js
+++ src/background.js
@@ -1,17 +1,17 @@
 browser.commands.onCommand.addListener(function (command) {
-	if (command === "increase") {
+	if (command === 'increase') {
 		applyPlayBackRate(0.1);
-	} else
-		if (command === "decrease") {
-			applyPlayBackRate(-0.1);
-		}
+	} else if (command === 'decrease') {
+		applyPlayBackRate(-0.1);
+	}
 });
 
 function applyPlayBackRate(amount) {
 	const executing = browser.tabs.executeScript({
-		code: "document.querySelectorAll('video').forEach(video => video.playbackRate += " +
-		amount +
-		")"
+		code:
+			"document.querySelectorAll('video').forEach(video => video.playbackRate += " +
+			amount +
+			')',
 	});
 
 	executing;
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,23 +1,28 @@
-document.getElementById("source").textContent = browser.i18n.getMessage("source");
-document.getElementById("author").textContent = browser.i18n.getMessage("author");
-document.getElementsByTagName("title")[0].textContent = browser.i18n.getMessage("extensionName");
+document.getElementById('source').textContent =
+	browser.i18n.getMessage('source');
+document.getElementById('author').textContent =
+	browser.i18n.getMessage('author');
+document.getElementsByTagName('title')[0].textContent =
+	browser.i18n.getMessage('extensionName');
 
-document.getElementsByTagName("html")[0].setAttribute("lang",
-	browser.i18n.getUILanguage()
-);
+document
+	.getElementsByTagName('html')[0]
+	.setAttribute('lang', browser.i18n.getUILanguage());
 
+document.addEventListener('contextmenu', (event) => event.preventDefault());
 
-document.addEventListener("contextmenu", event => event.preventDefault());
-
-
-["keyup", "change"].forEach(event =>
-	document.getElementById("speedInput").addEventListener(event, applyPlayBackRate)
+['keyup', 'change'].forEach((event) =>
+	document
+		.getElementById('speedInput')
+		.addEventListener(event, applyPlayBackRate)
 );
 
 function applyPlayBackRate() {
 	const executing = browser.tabs.executeScript({
-		code: "document.querySelectorAll('video').forEach(video => video.playbackRate =" +
-			document.getElementById("speedInput").value + ")"
+		code:
+			"document.querySelectorAll('video').forEach(video => video.playbackRate =" +
+			document.getElementById('speedInput').value +
+			')',
 	});
 
 	executing;
