90543bcb7f7de18be293576ebd6ba188866546d1
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-03-06 02:05:01 +0300

    Add options UI to store value to be used when increasing or decreasing

--- src/background.js
+++ src/background.js
@@ -1,18 +1,26 @@
-browser.commands.onCommand.addListener(function (command) {
-	if (command === 'increase') {
-		applyPlayBackRate(0.1);
-	} else if (command === 'decrease') {
-		applyPlayBackRate(-0.1);
+let increaseAmount;
+
+function getIncreaseAmount(restoredSettings) {
+	increaseAmount = restoredSettings.increaseAmount;
+	if (increaseAmount === undefined) {
+		increaseAmount = 0.1;
 	}
-});
+}
 
 function applyPlayBackRate(amount) {
 	const executing = browser.tabs.executeScript({
-		code:
-			"document.querySelectorAll('video').forEach(video => video.playbackRate += " +
-			amount +
-			')',
+		code: `document.querySelectorAll('video').forEach(video => video.playbackRate += ${amount})`,
 	});
 
 	executing;
 }
+
+browser.commands.onCommand.addListener(function (command) {
+	browser.storage.local.get().then(getIncreaseAmount);
+
+	if (command === 'increase') {
+		applyPlayBackRate(increaseAmount);
+	} else if (command === 'decrease') {
+		applyPlayBackRate(-increaseAmount);
+	}
+});
---
+++ src/options/options.js
@@ -0,0 +1,30 @@
+// Get settings from input and store them.
+function storeSettings() {
+	browser.storage.local.set({
+		increaseAmount: document.querySelector('#increaseAmount'),
+	});
+}
+
+function loadUI(restoredSettings) {
+	// Load localization.
+	document
+		.querySelector('html')
+		.setAttribute('lang', browser.i18n.getUILanguage());
+	document.getElementById('increaseAmountLable').textContent =
+		browser.i18n.getMessage('increaseAmountLable');
+	document.getElementById('saveButton').textContent =
+		browser.i18n.getMessage('saveButton');
+
+	// and set value in the input if there was no stored one.
+	const input = document.getElementById('increaseAmount');
+
+	if (restoredSettings.increaseAmount === undefined) {
+		input.value = 0.1;
+	} else {
+		input.value = restoredSettings.increaseAmount;
+	}
+}
+
+browser.storage.local.get().then(loadUI);
+
+document.getElementById('saveButton').addEventListener('click', storeSettings);
